a6759b1f0225e9d9e42cd39547aec206309f9512
QuangDuy101 <duy2k4ml1234@gmail.com>
2024-10-21 22:15:32 +0700

    init project

---
+++ IndentityService/src/test/java/com/quangduy/IndentityService/IndentityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quangduy.IndentityService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndentityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
